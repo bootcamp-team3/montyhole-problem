51ae7cabc8f3308df213b74d72c80132a4afd0da
mindw96 <mindw96@naver.com>
2024-01-15 20:20:22 +0900

    feat: Fix main.py

--- main.py
+++ main.py
@@ -3,42 +3,48 @@ from random import randint
 
 
 def pick_two_doors():
-    doors = range(1, 4)
-    no1 = randint(doors)
-    no2 = randint(doors)
+    no1 = randint(1,4)
+    no2 = randint(1,4)
 
     return no1, no2
   
-def switch_choice(first,car):
+def switch_choice(first,car, switch=True):
     remain =[d for d in [1,2,3] if d not in [first,car]]
     open_door = random.choice(remain)
-
-    can_choice = [d for d in [1,2,3] if d not in [first,open_door]]
-    second_choice = can_choice[0]
+    
+    if switch:
+        can_choice = [d for d in [1,2,3] if d not in [first,open_door]]
+        second_choice = can_choice[0]
+    else:
+        second_choice = first
 
     return first, car, second_choice
 
-def check_result(switched_choice, initial_choice, car_door):
-    stay_win = 0
-    switched_win = 0
+def check_result(choice, car_door):
+    win = 0
     
-    if(initial_choice == car_door):
-        stay_win = 1
-    elif(switched_choice == car_door):
-        switched_win=1
+    if(choice == car_door):
+        win=1
         
-    return stay_win, switched_win
+    return win
 
 
 if __name__=='__main__':
   stay, switch = 0, 0
-  for _ in range(1000):
-    car, first = picj_two_doors()
-    first, car, second_choice = switch_choice(first, car)
-    stay_win, switched_win = check_result(second_choice, first, car)
+  num = 1000000  
+  for _ in range(num):
+    car, first = pick_two_doors()
+    first, car, second_choice = switch_choice(first, car, True)
+    win = check_result(second_choice, car)
     
-    stay += stay_win
-    switch += switched_win
+    switch += win
   
-  print('바꿨을 때의 승률: {}'.format(switch/1000 * 100))
-  print('안바궜을 떄의 승률: {}'.format(stay/1000 * 100))
+  print('바꿨을 때의 승리: {}, 패배: {}, 승률: {}'.format(switch, num - switch, switch/num * 100 ))
+  
+  for _ in range(num): 
+    car, first = pick_two_doors()
+    first, car, second_choice = switch_choice(first, car, False)
+    win = check_result(second_choice, car)
+    
+    stay += win
+  print('안바궜을 때의 승리: {}, 패배: {},  승률: {}'.format(stay, num - stay, stay/num * 100))
